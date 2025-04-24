9f948b98986c97db005361e5d72171e7413182d8
raj-008 <rajshingala34@gmail.com>
2025-04-24 20:44:00 +0530

    Refactor : Add Validation for Conatct Form

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -15,15 +15,14 @@ const About = ({ aboutRef }) => {
               ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis asperiores, exercitationem eius nostrum
               consequuntur iure aliquam itaque, assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus soluta porro reprehenderit eos inventore facere, fugit, molestiae ab
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam, aliquam dolore excepturi quae. Distinctio
-              enim at eligendi perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem, at ab sequi qui modi delectus quia corrupti alias distinctio nostrum. Minima
-              ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis asperiores, exercitationem eius nostrum
-              consequuntur iure aliquam itaque, assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus soluta porro reprehenderit eos inventore facere, fugit, molestiae ab.
+              enim at eligendi perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem, at ab sequi qui modi delectus quia corrupti alias distinctio nostrum Minima
+              ex dolor.
             </p>
           </div>
 
           <div className="about-img-container">
             <img
-              src="https://media.istockphoto.com/id/1288148699/photo/young-business-woman-in-office-stock-photo.jpg?s=2048x2048&w=is&k=20&c=INLng5rZnkj7fzRHCWp_As_INJB0MN6L0Z27uk1gqPY="
+              src="about_us.jpg"
               alt="about_us"
               className="about-img"
             />
--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
@@ -10,63 +10,138 @@ import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import MailIcon from "@mui/icons-material/Mail";
 import { Element } from "react-scroll";
-
-const card = (
-  <CardContent>
-    <Typography variant="h5" component="div" className="contact-title">
-      <span> Contact</span>
-    </Typography>
-    <div sx={{ my: 1.5 }} color="text.secondary" className="form-container">
-      <form>
-        <TextField className="contact-input" name="name" style={{ width: "100%" }} label="Name" variant="filled" />
-        <TextField className="contact-input contact-email" name="email" style={{ width: "100%" }} label="Email" variant="filled" />
-
-        <TextField className="contact-input contact-phone" name="phone" style={{ width: "100%" }} label="Phone" variant="filled" />
-
-        <textarea className="contact-input input-inquiry" rows={8} placeholder="Enter Your Message"></textarea>
-
-        <div className="button-wrapper">
-          <Button variant="contained" className="inquiry-submit">
-            Send
-          </Button>
-        </div>
-      </form>
-    </div>
-  </CardContent>
-);
+import { errorToaster, successToaster } from "../../Utils/Toasters.utils";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+
+  const {register, handleSubmit, reset, formState: { errors }} = useForm();
+
+  const [formKey, setFormKey] = useState(0);
+
+  const onSubmit = async () => {
+    try {
+      successToaster("Your message has been recived, we'll get back to you soon");
+      reset();
+      setFormKey(prev => prev + 1);
+  
+    } catch (error) {
+      errorToaster(error.response.data.message);
+    }
+  };
+
   return (
     <>
-    <Element name="contact">
-      <div className="contact">
-        <div className="contact-details">
-          <div className="contact-conent">
-            <div className="contact-item phone">
-              <LocalPhoneIcon className="contact-icon" />
-              +91 7894561230
-            </div>
+      <Element name="contact">
+        <div className="contact">
+          <div className="contact-details">
+            <div className="contact-conent">
+              <div className="contact-item phone">
+                <LocalPhoneIcon className="contact-icon" />
+                +91 7894561230
+              </div>
 
-            <div className="contact-item location">
-              <LocationOnIcon className="contact-icon" />
-              A-1 Brick Lane, London
-            </div>
+              <div className="contact-item location">
+                <LocationOnIcon className="contact-icon" />
+                A-1 Brick Lane, London
+              </div>
 
-            <div className="contact-item mail">
-              <MailIcon className="contact-icon" />
-              rajshingala07@gmail.com
+              <div className="contact-item mail">
+                <MailIcon className="contact-icon" />
+                rajshingala07@gmail.com
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="contact-form">
-          {
+          <div className="contact-form">
             <Box sx={{ minWidth: 275 }} style={{ maxWidth: "100%", maxHeight: "100%" }}>
-              <Card variant="outlined">{card}</Card>
+              <Card variant="outlined">
+                <CardContent>
+                  <Typography variant="h5" component="div" className="contact-title">
+                    <span> Contact</span>
+                  </Typography>
+
+                  <div sx={{ my: 1.5 }} color="text.secondary" className="form-container">
+                    <form key={formKey} onSubmit={handleSubmit(onSubmit)}>
+                      <TextField className="contact-input" name="name" style={{ width: "100%" }} label="Name" variant="filled"
+                        slotProps={{
+                          input: {
+                            disableUnderline: true,
+                          },
+                        }}
+                        {...register("name", {
+                          required: { value: true, message: "Name is required" },
+                          pattern: {
+                            value: /^[A-Za-z\s]+$/,
+                            message: "Only text are allowed",
+                          },
+                        })}
+                        helperText={errors.name ? errors.name.message : ""}
+                        error={!!errors.name}
+                      />
+
+                      <TextField className="contact-input contact-email" name="email" style={{ width: "100%" }} label="Email" variant="filled"
+                        slotProps={{
+                          input: {
+                            disableUnderline: true,
+                          },
+                        }}
+                        {...register("email", {
+                          required: { value: true, message: "Email is required" },
+                          pattern: {
+                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                            message: "Enter a valid email address",
+                          },
+                        })}
+                        helperText={errors.email ? errors.email.message : ""}
+                        error={!!errors.email}
+                      />
+
+                      <TextField className="contact-input contact-phone" name="phone" style={{ width: "100%" }} label="Phone" variant="filled"
+                        slotProps={{
+                          input: {
+                            disableUnderline: true,
+                          },
+                        }}
+                        {...register("phone", {
+                          pattern: {
+                            value: /^(?:\+91|91|0)?[6-9]\d{9}$/,
+                            message: "Enter a valid Indian phone number",
+                          },
+                        })}
+                        helperText={errors.phone ? errors.phone.message : ""}
+                        error={!!errors.phone}
+                      />
+
+                      <TextField id="filled-multiline-static" label="Your Message" multiline style={{ width: "100%" }} rows={8} name="message" variant="filled" className="contact-input input-inquiry"
+                        slotProps={{
+                          input: {
+                            disableUnderline: true,
+                          },
+                        }}
+                        {...register("message", {
+                          required: { value: true, message: "Message is required" },
+                          pattern: {
+                            value: /^[A-Za-z0-9\s]+$/,
+                            message: "Only letters, numbers, and spaces are allowed",
+                          },
+                        })}
+                        helperText={errors.message ? errors.message.message : ""}
+                        error={!!errors.message}
+                      />
+
+                      <div className="button-wrapper">
+                        <Button variant="contained" className="inquiry-submit" type="submit">
+                          Send
+                        </Button>
+                      </div>
+                    </form>
+                  </div>
+                </CardContent>
+              </Card>
             </Box>
-          }
+          </div>
         </div>
-      </div>
       </Element>
     </>
   );
