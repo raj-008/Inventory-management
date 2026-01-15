185457a8da74b6c32d20e7143e79cafdef6b5101
raj-008 <rajshingala34@gmail.com>
2026-01-15 13:21:03 +0530

    Refactor : Update the About Section

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -4,32 +4,37 @@ import { Element } from "react-scroll";
 
 const About = ({ aboutRef }) => {
   return (
-    <>
-      <Element name="about">
-        <div className="about" ref={aboutRef}>
-          <div className="about-text">
-            <div className="about-title">About</div>
-            <p className="about-content-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam, aliquam dolore excepturi quae. Distinctio
-              enim at eligendi perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem, at ab sequi qui modi delectus quia corrupti alias distinctio nostrum. Minima
-              ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis asperiores, exercitationem eius nostrum
-              consequuntur iure aliquam itaque, assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus soluta porro reprehenderit eos inventore facere, fugit, molestiae ab
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam, aliquam dolore excepturi quae. Distinctio
-              enim at eligendi perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem, at ab sequi qui modi delectus quia corrupti alias distinctio nostrum Minima
-              ex dolor.
-            </p>
-          </div>
+    <Element name="about">
+      <div className="about" ref={aboutRef}>
+        {/* LEFT: TEXT */}
+        <div className="about-text">
+          <div className="about-title">About</div>
+          <p className="about-content-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde fugit veniam eius,
+            perspiciatis sunt? Corporis qui ducimus quibusdam, aliquam dolore excepturi quae.
+            Distinctio enim at eligendi perferendis in cum quibusdam sed quae.
+          </p>
+        </div>
 
-          <div className="about-img-container">
+        {/* RIGHT: IMAGE */}
+        <div className="about-image-section">
+          <div className="decorative-circle"></div>
+
+          <div className="about-image-wrapper">
             <img
               src="about_us.png"
-              alt="about_us"
-              className="about-img"
+              alt="Team collaboration and business growth"
+              className="about-image"
+              onError={(e) => {
+                e.target.style.display = "none";
+                e.target.parentElement.innerHTML =
+                  '<div class="image-fallback">📊</div>';
+              }}
             />
           </div>
         </div>
-      </Element>
-    </>
+      </div>
+    </Element>
   );
 };
 
