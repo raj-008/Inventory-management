97810930141efa4bd32785b2090cb5f8667f5350
raj-008 <rajshingala34@gmail.com>
2026-01-16 17:02:24 +0530

    Refactor : Configure Preview Site

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3003, // PM2 port
+    allowedHosts: ["inventory.rajshingala.site", "localhost", "127.0.0.1"],
+  },
 });
